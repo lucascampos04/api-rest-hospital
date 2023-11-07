7102cae4983f1e19063644983db15b6d8fbbf7c1
Lucascampos04 <camposdlucasoli@gmail.com>
2023-11-07 18:04:53 -0300

    deixando

---
+++ src/main/java/com/hospital/apihospital/Model/DTO/PacienteDTO.java
@@ -0,0 +1,19 @@
+package com.hospital.apihospital.Model.DTO;
+
+import com.hospital.apihospital.Model.Entity.CadastrarPaciente;
+
+public record PacienteDTO(Long id, String nome, String cpf, String rg) {
+    public static PacienteDTO fromEntity(CadastrarPaciente paciente){
+        return new PacienteDTO(paciente.getId(), paciente.getNome(), paciente.getCpf(), paciente.getRg());
+    }
+    public PacienteDTO(Long id, String nome){
+        this(id, nome, null, null);
+    }
+
+    public PacienteDTO(Long id, String nome, String cpf, String rg) {
+        this.id = id;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.rg = rg;
+    }
+}
