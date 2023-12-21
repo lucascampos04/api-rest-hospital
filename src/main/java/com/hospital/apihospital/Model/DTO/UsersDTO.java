5da8496b87921ee46def988010f69d41d51d4129
lucascampos04 <camposdlucasoli@gmail.com>
2023-12-21 11:27:00 -0300

    feat : new message of error and functionalities in validations

--- src/main/java/com/hospital/apihospital/Model/DTO/UsersDTO.java
+++ src/main/java/com/hospital/apihospital/Model/DTO/UsersDTO.java
@@ -5,32 +5,75 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.hospital.apihospital.Model.Entity.CadastrarUsers;
 import com.hospital.apihospital.Model.Enum.CargoEnum;
 import com.hospital.apihospital.Model.Enum.RoleEnum;
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 import java.util.Date;
 
 @NoArgsConstructor
 @Data
 public class UsersDTO {
     private Long id;
-    private String nome;
-    private String cpf;
-    private String rg;
-    private Date dataNascimento;
-    private String genero;
-    private String email;
-    private String telefone;
-    private Date dataRegistro;
-    private String plano_paciente;
-    private RoleEnum role;
 
+    @NotBlank(message = "O nome é obrigatório")
+    @Pattern(regexp = "^[^0-9]+$", message = "O nome não deve conter números")
+    private String nome;
+
+    @Column(name = "cpf", length = 19, unique = true)
+    @NotBlank(message = "O CPF é obrigatório")
+    private String cpf;
+
+    @Column(name = "rg", length = 15, unique = true)
+    @NotBlank(message = "O RG é obrigatório")
+    private String rg;
+
+    @Column(name = "data_nascimento")
+    @Temporal(TemporalType.DATE)
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @NotBlank(message = "A data de nascimento é obrigatorio")
+    private Date dataNascimento;
+
+    @Column(name = "genero", length = 30)
+    @NotBlank(message = "Genero obrigatorio")
+    @Pattern(regexp = "^[^0-9]+$", message = "O o genero não deve conter números")
+    private String genero;
+
+    @Column(name = "email")
+    @NotBlank
+    private String email;
+
+    @Column(name = "telefone")
+    @NotBlank
+    private String telefone;
+
+    @Column(name = "data_registro")
+    @Temporal(TemporalType.TIMESTAMP)
+    @NotBlank(message = "A data de registro é obrigatória")
+    private Date dataRegistro;
+
+    @Column(name = "plano_paciente", length = 50)
+    @NotBlank
+    private String plano_paciente;
+
+    @JsonIgnore
+    @Column(name = "cargo")
+    @Enumerated(EnumType.STRING)
     @JsonInclude(JsonInclude.Include.NON_NULL)
     private CargoEnum cargo;
-    
+
+    @Column(name = "salario")
+    @JsonIgnore
     @JsonInclude(JsonInclude.Include.NON_NULL)
     private Double salario;
+
+    @Column(name = "role", length = 20)
+    @NotBlank(message = "A role é obrigatória")
+    @Pattern(regexp = "^[^0-9]+$", message = "O role não deve conter números")
+    private RoleEnum role;
 
     public UsersDTO(Long id,
                     String nome,
--- src/main/java/com/hospital/apihospital/Model/Entity/CadastrarUsers.java
+++ src/main/java/com/hospital/apihospital/Model/Entity/CadastrarUsers.java
@@ -1,18 +1,24 @@
 package com.hospital.apihospital.Model.Entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.hospital.apihospital.Model.Enum.CargoEnum;
 import com.hospital.apihospital.Model.Enum.RoleEnum;
 import jakarta.persistence.*;
-import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 import java.util.Date;
-import java.util.List;
 
 @Entity
 @Table(name = "cadastrarUsers")
-@Data
+@Getter
+@Setter
+@NoArgsConstructor
 public class CadastrarUsers {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -20,23 +26,26 @@ public class CadastrarUsers {
 
     @Column(name = "nome", length = 80)
     @NotBlank(message = "O nome é obrigatório")
+    @Pattern(regexp = "^[^0-9]+$", message = "O nome não deve conter números")
     private String nome;
 
-    @Column(name = "rg", length = 15, unique = true, nullable = false)
+    @Column(name = "rg", length = 15, unique = true)
     @NotBlank(message = "O RG é obrigatório")
     private String rg;
 
-    @Column(name = "cpf", length = 20, unique = true, nullable = false)
+    @Column(name = "cpf", length = 19, unique = true)
     @NotBlank(message = "O CPF é obrigatório")
     private String cpf;
 
     @Column(name = "data_nascimento")
     @Temporal(TemporalType.DATE)
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     @NotBlank(message = "A data de nascimento é obrigatorio")
     private Date dataNascimento;
 
     @Column(name = "genero", length = 30)
     @NotBlank(message = "Genero obrigatorio")
+    @Pattern(regexp = "^[^0-9]+$", message = "O o genero não deve conter números")
     private String genero;
 
     @Column(name = "plano_paciente", length = 50)
@@ -56,19 +65,20 @@ public class CadastrarUsers {
     @NotBlank(message = "A data de registro é obrigatória")
     private Date dataRegistro;
 
-    @JsonIgnore
     @Column(name = "cargo")
     @Enumerated(EnumType.STRING)
+    @JsonIgnore
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private CargoEnum cargo;
 
     @Column(name = "salario")
     @JsonIgnore
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private Double salario;
 
     @Column(name = "role", length = 20)
     @NotBlank(message = "A role é obrigatória")
+    @Pattern(regexp = "^[^0-9]+$", message = "O role não deve conter números")
     private RoleEnum role;
-
-
 
 }
