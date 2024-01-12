4765906e11291827312647ceeb83d937fe0d0ac2
Lucas Oliveira Campos <camposdlucasoli@gmail.com>
2024-01-12 16:37:53 -0300

    feat : Discount on working plans

--- src/main/java/com/hospital/apihospital/services/ConsultasServices/CadastrarConsultaService.java
+++ src/main/java/com/hospital/apihospital/services/ConsultasServices/CadastrarConsultaService.java
@@ -5,29 +5,34 @@ import com.hospital.apihospital.Model.Entity.CadastrarUsers;
 import com.hospital.apihospital.Model.Entity.ConsultaEntity;
 import com.hospital.apihospital.Model.Repository.ConsultaRepository;
 import com.hospital.apihospital.Model.Repository.UsersRepository;
+import com.hospital.apihospital.services.DescontoEmPlanos.PlanoService;
 import com.hospital.apihospital.services.SendEmail.NotificationConsulta;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class CadastrarConsultaService {
     private final ConsultaRepository consultaRepository;
     private final NotificationConsulta notificationConsulta;
     private final UsersRepository usersRepository;
+    private final PlanoService planoService;
 
     @Autowired
     public CadastrarConsultaService(ConsultaRepository consultaRepository, NotificationConsulta notificationConsulta,
-                                    UsersRepository usersRepository) {
+                                    UsersRepository usersRepository, PlanoService planoService) {
         this.consultaRepository = consultaRepository;
         this.notificationConsulta = notificationConsulta;
         this.usersRepository = usersRepository;
+        this.planoService = planoService;
     }
 
     public ConsultaDTO cadastrarConsulta(ConsultaDTO consultaDTO) {
         ConsultaEntity consultaEntity = new ConsultaEntity();
         consultaEntity.setTipoConsulta(consultaDTO.getTipoConsulta());
         consultaEntity.setData(consultaDTO.getData());
-        consultaEntity.setValor(consultaDTO.getValor());
+        consultaEntity.setValor(calcularDesconto(consultaDTO));
 
         CadastrarUsers users = usersRepository.findById(consultaDTO.getUserId()).orElse(null);
         consultaEntity.setUsers(users);
@@ -35,6 +40,12 @@ public class CadastrarConsultaService {
         ConsultaEntity consultaSalva = consultaRepository.save(consultaEntity);
 
         return convertEntityToDTO(consultaSalva);
+    }
+
+
+    private double calcularDesconto(ConsultaDTO consultaDTO){
+        CadastrarUsers usuario = usersRepository.findById(consultaDTO.getUserId()).orElse(null);
+        return planoService.calcularValorComDesconto(usuario, consultaDTO.getValor());
     }
 
     private ConsultaDTO convertEntityToDTO(ConsultaEntity entity) {
